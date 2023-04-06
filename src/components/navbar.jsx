2183e20eb0d26e460bd756dd3eece3385e51cf1e
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2023-04-05 21:48:35 -0400

    finished up navbar, not rendering have to find error

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Logo from "../assets/Hannah.png";
 import {
-  Fabars,
+  FaBars,
   FaTimes,
   FaGithub,
   FaLinkedin,
@@ -14,6 +14,7 @@ const Navbar = () => {
   const handleClick = () => setNavi(!navi);
 
   return (
+    <div>
     <div className="z-50 fixed w-full h-[150px] flex justify-between items-center px-4 bg-[#333333] text-[#AD8F18] text 2-xl">
       <a href=".../components/home.jsx">
         <img
@@ -48,7 +49,28 @@ const Navbar = () => {
       </div>
     </div>
 
-    
+    <div onCLick={handleClick} className="md:hidden z-20">
+        {!navi? <FaBars /> : <FaTimes />}
+    </div>
+    {/* this is going to be a hamburger menu for when the screen shrinks */}
+    <ul
+        className={
+            !navi
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center "
+        }
+        >
+        <li>
+            <Link to="about" smooth={true} duration={500}>About"</Link>
+        </li>
+        <li>
+            <Link to="applications" smooth={true} duration={500}>Applications</Link>
+        </li>
+        <li>
+            <Link to="contact" smooth={true} duration={500}>Contact</Link>
+        </li>
+    </ul>
+    </div>
   );
 };
 
