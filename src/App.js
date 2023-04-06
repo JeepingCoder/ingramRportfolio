b57bcea38f18bf6ee6e0324cecd38caf1a49a6fe
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2023-04-05 21:04:40 -0400

    added navbar styling and my logo to top left of page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import About from "./components/about";
+import Navbar from "./components/navbar"
 
 
 function App () {
   return (
     <div>
       <About />
+      <Navbar />
     </div>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,2 +1,26 @@
 import React, {useState} from "react";
-import Logo from "../assets/Hannah.png"
+import Logo from "../assets/Hannah.png";
+
+
+
+const Navbar = () => {
+    const [navi, setNavi] = useState (false);
+    const handleClick = () => setNavi (!navi);
+
+
+    return (
+        <div className="z-50 fixed w-full h-[150px] flex justify-between items-center px-4 bg-[#333333] text-[#AD8F18] text 2-xl">
+            <a href=".../components/home.jsx">
+                <img className="rounded-full"
+                src={Logo} alt="logo" style={{ width: "155px" }} />
+            </a>
+        </div>
+
+        // nav bar link top of page
+    )
+};
+
+
+
+
+export default Navbar;
