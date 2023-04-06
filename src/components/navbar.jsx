90f655054c8c38830b010e2681a1e874da7688bd
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2023-04-05 21:29:40 -0400

    added links to top of navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,55 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Logo from "../assets/Hannah.png";
-
-
+import {
+  Fabars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-    const [navi, setNavi] = useState (false);
-    const handleClick = () => setNavi (!navi);
+  const [navi, setNavi] = useState(false);
+  const handleClick = () => setNavi(!navi);
 
+  return (
+    <div className="z-50 fixed w-full h-[150px] flex justify-between items-center px-4 bg-[#333333] text-[#AD8F18] text 2-xl">
+      <a href=".../components/home.jsx">
+        <img
+          className="rounded-full"
+          src={Logo}
+          alt="logo"
+          style={{ width: "155px" }}
+        />
+      </a>
+      <div>
+        <ul className="md:flex">
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
 
-    return (
-        <div className="z-50 fixed w-full h-[150px] flex justify-between items-center px-4 bg-[#333333] text-[#AD8F18] text 2-xl">
-            <a href=".../components/home.jsx">
-                <img className="rounded-full"
-                src={Logo} alt="logo" style={{ width: "155px" }} />
-            </a>
-        </div>
+          <li>
+            <Link to="applications" smooth={true} duration={500}>
+              Applications
+            </Link>
+          </li>
 
-        // nav bar link top of page
-    )
+          <li>
+            < Link to="contact" smooth={true} duration={500}>Contact</Link>
+          </li>
+          {/* build in a field for a resume download */}
+          <li>
+            <Link to="contact" smooth={true} duration={500}>Resume</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    
+  );
 };
-
-
-
 
 export default Navbar;
